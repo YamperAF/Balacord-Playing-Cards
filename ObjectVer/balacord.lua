385d0da78001136b3425bdb0fc2282ba1d7f70f0
Okay <183864149+OOkayOak@users.noreply.github.com>
2024-11-06 23:21:27 +0000

    Object version
    Uploaded the Object version of BalacordPlayingCards

---
+++ ObjectVer/balacord.lua
@@ -0,0 +1,103 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Balacord Playing Cards
+--- MOD_ID: BalaCards
+--- MOD_AUTHOR: [YamperAF, Opal]
+--- MOD_DESCRIPTION: Adds a Deck Skin for each Suit based on Balatro Discord members.
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-0812d]
+--- PREFIX: balacards
+----------------------------------------------
+------------MOD CODE ------------------------- 
+
+SMODS.Atlas{
+    key = 'balacord_spades_atlas',
+    path = 'balacord_spades.png',
+    px = 71,
+    py = 95,
+    atlas_table = 'ASSET_ATLAS'
+}
+
+SMODS.Atlas{
+    key = 'balacord_hearts_atlas',
+    path = 'balacord_hearts.png',
+    px = 71,
+    py = 95,
+    atlas_table = 'ASSET_ATLAS'
+}
+
+SMODS.Atlas{
+    key = 'balacord_diamonds_atlas',
+    path = 'balacord_diamonds.png',
+    px = 71,
+    py = 95,
+    atlas_table = 'ASSET_ATLAS'
+}
+
+SMODS.Atlas{
+    key = 'balacord_clubs_atlas',
+    path = 'balacord_clubs.png',
+    px = 71,
+    py = 95,
+    atlas_table = 'ASSET_ATLAS'
+}
+
+SMODS.DeckSkin{
+    key = 'balacord_spades',
+    suit = 'Spades',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+    },
+    lc_atlas = 'balacord_spades_atlas',
+    loc_txt = {
+        ['en_us'] = 'Balacord'
+    },
+    posStyle = 'suit'
+}
+
+SMODS.DeckSkin{
+    key = 'balacord_hearts',
+    suit = 'Hearts',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+    },
+    lc_atlas = 'balacord_hearts_atlas',
+    loc_txt = {
+        ['en_us'] = 'Balacord'
+    },
+    posStyle = 'suit'
+}
+
+SMODS.DeckSkin{
+    key = 'balacord_clubs',
+    suit = 'Clubs',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+    },
+    lc_atlas = 'balacord_clubs_atlas',
+    loc_txt = {
+        ['en_us'] = 'Balacord'
+    },
+    posStyle = 'suit'
+}
+
+SMODS.DeckSkin{
+    key = 'balacord_diamonds',
+    suit = 'Diamonds',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+    },
+    lc_atlas = 'balacord_diamonds_atlas',
+    loc_txt = {
+        ['en_us'] = 'Balacord'
+    },
+    posStyle = 'suit'
+}
+
+SMODS.Joker:take_ownership('j_joker', {
+
+    atlas = "yamperjimbo",
+    pos = {x = 0, y = 0}
+
+})
+
+----------------------------------------------
+------------MOD CODE END----------------------
